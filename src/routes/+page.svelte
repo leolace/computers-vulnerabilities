eff166ddce26fb21b944f7835978a8d8e0e0ae88
leolace <lgonsalez02@gmail.com>
2024-04-25 00:10:45 -0300

    add sections and fix bugs

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,18 +6,23 @@
 	let startYear = 1950;
 	const SCROLL_AMOUNT = 20;
 	const SCROLL_OFFSET = 600;
+	const MAX_SCROLL = 500;
+	let slot_size = 2000;
+
+	const contents = [{ title: 'Titulo 1' }, { title: 'Titulo 2' }, { title: 'teste' }];
 
 	onMount(() => {
 		window.scrollTo(0, 0);
+		console.log(document.body.scrollWidth);
+		// slot_size = document.body.scrollWidth / 5;
 		const scroll = (e: WheelEvent) => {
 			if (e.deltaY < 0) {
 				if (scrollX - SCROLL_AMOUNT < 0) return;
 				scrollX -= SCROLL_AMOUNT;
 				window.scrollTo({ left: scrollX, top: 0, behavior: 'smooth' });
 			} else {
-				if (scrollX >= document.body.scrollWidth - 510) return;
+				if (scrollX >= document.body.scrollWidth - MAX_SCROLL) return;
 				scrollX += SCROLL_AMOUNT;
-				console.log(document.body.scrollWidth);
 				if (scrollX < SCROLL_OFFSET) return;
 				window.scrollTo({ left: scrollX, top: 0, behavior: 'smooth' });
 			}
@@ -30,24 +35,30 @@
 	});
 
 	const resetTimeline = () => {
+	  const DECREMENT_RATE = 50;
+
 		let id = setInterval(() => {
-			scrollX -= 10;
-			if (scrollX <= 0) clearInterval(id);
+			if (scrollX <= DECREMENT_RATE) return clearInterval(id);
+			scrollX -= DECREMENT_RATE;
 		}, 1);
 		window.scrollTo({ left: 0, top: 0, behavior: 'smooth' });
 	};
 </script>
 
 <main>
-	<div class="content">
-		<h1>titulo da pagina!</h1>
+	<div class="container">
+		{#each contents as content}
+			<div class="content" style={`max-width: ${slot_size}px`}>
+				<h1>{content.title}</h1>
+			</div>
+		{/each}
 	</div>
 
 	<div class="timeline-container">
 		<div class="timeline">
 			<div class="marker-container" style={`left: ${scrollX}px`}>
 				<span class="marker"></span>
-				<span class="text">{Math.trunc(startYear + scrollX / 500)}</span>
+				<span class="text">{Math.trunc(startYear + scrollX / slot_size)}</span>
 			</div>
 		</div>
 		<div class="reset" style={`left: ${scrollX}px`}>
@@ -63,8 +74,8 @@
 
 	main {
 		position: relative;
-		height: 90dvh;
-		width: 300rem;
+		height: 100dvh;
+		width: 400rem;
 		overflow-x: hidden;
 		overflow-y: hidden;
 	}
@@ -74,6 +85,10 @@
 		left: 30rem;
 		bottom: 10%;
 		width: 100%;
+
+		@media (max-width: 900px) {
+			left: 5rem;
+		}
 	}
 
 	.reset {
@@ -100,8 +115,10 @@
 	.text {
 		position: absolute;
 		color: #fff;
-		font-size: 1.5rem;
-		text-align: center;
+		font-size: 5rem;
+		top: -5rem;
+		font-family: monospace;
+		transform: translateX(-50%);
 	}
 
 	.marker {
@@ -110,5 +127,19 @@
 		height: 1.25rem;
 		background-color: #fff;
 		border-radius: 50%;
+	}
+
+	.container {
+		padding: 1rem;
+		display: flex;
+		height: 100%;
+	}
+
+	.content {
+		flex: 1;
+		height: 100%;
+		transform: translateX(30rem);
+		max-width: 50rem;
+		border-right: 1px solid red;
 	}
 </style>
