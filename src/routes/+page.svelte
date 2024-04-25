0f460389a7a618ddde78d73dc3419f578ce62486
leolace <lgonsalez02@gmail.com>
2024-04-25 14:46:35 -0300

    Suport mobile screens and improve other stuffs

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,35 +6,76 @@
 	let startYear = 1950;
 	let slotSize = 2000;
 
-	const SCROLL_AMOUNT = 10;
-	const SCROLL_SPEED = 2;
+	const SCROLL_AMOUNT = 100;
 	const SCROLL_OFFSET = 600;
 	const MAX_SCROLL = 500;
 	const MARGIN_LEFT = 130;
+	const SCROLL_SPEED = 0.0;
+	const SCROLL_ACCELERATION = SCROLL_AMOUNT / (SCROLL_SPEED * 10000);
+	let scrolling_right = false;
+	let scrolling_left = false;
 
 	const contents = [{ title: 'Titulo 1' }, { title: 'Titulo 2' }, { title: 'teste' }];
 
+	const goLeft = () => {
+		scrolling_right = false;
+		const beforeScrollX = scrollX - SCROLL_AMOUNT;
+		let id = setInterval(() => {
+			const isLimit = scrollX <= 0;
+			if (beforeScrollX > scrollX || isLimit || scrolling_right) return clearInterval(id);
+			scrolling_left = true;
+
+			window.scrollTo({ left: scrollX - window.innerWidth / 3, top: 0, behavior: 'instant' });
+			scrollX -= 1;
+		}, SCROLL_ACCELERATION);
+	};
+
+	const goRight = () => {
+		scrolling_left = false;
+		if (scrollX >= document.body.scrollWidth - MAX_SCROLL) return;
+		const beforeScrollX = scrollX + SCROLL_AMOUNT;
+		let id = setInterval(() => {
+			const isLimit = scrollX >= document.body.scrollWidth - MAX_SCROLL;
+			if (beforeScrollX < scrollX || isLimit || scrolling_left) return clearInterval(id);
+			scrolling_right = true;
+
+			if (scrollX < window.innerWidth / 3) {
+				scrollX += 1;
+				return;
+			}
+			window.scrollTo({ left: scrollX - window.innerWidth / 3, top: 0, behavior: 'instant' });
+			scrollX += 1;
+		}, SCROLL_ACCELERATION);
+	};
+
 	onMount(() => {
 		window.scrollTo(0, 0);
-		console.log(slotSize * (window.innerWidth / 1000));
 
 		const scroll = (e: WheelEvent) => {
 			console.log(scrollX);
 			if (e.deltaY < 0) {
-				if (scrollX - SCROLL_AMOUNT < 0) return;
-				scrollX -= SCROLL_AMOUNT * SCROLL_SPEED;
-				window.scrollTo({ left: scrollX, top: 0, behavior: 'smooth' });
+				goLeft();
 			} else {
-				if (scrollX >= document.body.scrollWidth - MAX_SCROLL) return;
-				scrollX += SCROLL_AMOUNT * SCROLL_SPEED;
-				if (scrollX < SCROLL_OFFSET) return;
-				window.scrollTo({ left: scrollX, top: 0, behavior: 'smooth' });
+				goRight();
 			}
 		};
 		document.body.addEventListener('wheel', scroll);
 
+		document.body.addEventListener('touchstart', () => {
+			document.body.style.overflowX = 'hidden';
+		});
+
+		document.body.addEventListener('touchend', () => {
+			document.body.style.overflowX = 'auto';
+		});
+
 		return () => {
 			document.body.removeEventListener('wheel', scroll);
+			document.body.removeEventListener(
+				'touchstart',
+				() => (document.body.style.overflowX = 'hidden')
+			);
+			document.body.removeEventListener('touchend', () => (document.body.style.overflowX = 'auto'));
 		};
 	});
 
@@ -42,34 +83,44 @@
 		const DECREMENT_RATE = 50;
 
 		let id = setInterval(() => {
-			if (scrollX <= DECREMENT_RATE) return clearInterval(id);
+			if (scrollX <= 0) {
+				scrollX = 0;
+				return clearInterval(id);
+			}
 			scrollX -= DECREMENT_RATE;
 		}, 1);
-		window.scrollTo({ left: 0, top: 0, behavior: 'smooth' });
+		window.scrollTo({ left: 0, top: 0, behavior: 'instant' });
 	};
 </script>
 
 <!--TODO: separate in components -->
 <main>
-	<div class="container" style={`left: ${MARGIN_LEFT}px`}>
-		{#each contents as content}
-			<div class="content" style={`max-width: ${slotSize}px`}>
-				<h1>{content.title}</h1>
-			</div>
-		{/each}
+	<div class="navigation">
+		<button class="left-btn" on:click={goLeft}>left</button>
+		<button class="right-btn" on:click={goRight}>right</button>
 	</div>
 
-	<div class="timeline-container" style={`left: ${MARGIN_LEFT}px`}>
-		<div class="timeline">
-			<div class="marker-container" style={`left: ${scrollX}px`}>
-				<span class="marker"></span>
-				<span class="text">{Math.trunc(startYear + scrollX / slotSize)}</span>
+	<section class="scroll-container">
+		<div class="container" style={`left: ${MARGIN_LEFT}px`}>
+			{#each contents as content}
+				<div class="content" style={`max-width: ${slotSize}px`}>
+					<h1>{content.title}</h1>
+				</div>
+			{/each}
+		</div>
+
+		<div class="timeline-container" style={`left: ${MARGIN_LEFT}px`}>
+			<div class="timeline">
+				<div class="marker-container" style={`left: ${scrollX}px`}>
+					<span class="marker"></span>
+					<span class="text">{Math.trunc(startYear + scrollX / slotSize)}</span>
+				</div>
+			</div>
+			<div class="reset" style={`left: ${scrollX}px`}>
+				<button on:click={resetTimeline}>voltar</button>
 			</div>
 		</div>
-		<div class="reset" style={`left: ${scrollX}px`}>
-			<button on:click={resetTimeline}>voltar</button>
-		</div>
-	</div>
+	</section>
 </main>
 
 <style lang="scss">
@@ -79,12 +130,34 @@
 
 	main {
 		position: relative;
-		height: 100dvh;
-		overflow-x: hidden;
-		overflow-y: hidden;
 
-	// TODO: define a dinamic width
+		// TODO: define a dinamic width
+		height: 100dvh;
+	}
+
+	.scroll-container {
+		height: 100dvh;
+		position: relative;
 		width: 400rem;
+	}
+
+	.navigation {
+		position: fixed;
+		left: 0;
+		bottom: 0;
+		display: flex;
+		width: 100%;
+		justify-content: space-between;
+		padding: 1rem;
+		z-index: 10;
+
+		button {
+			border: 1px solid #fff;
+			font-size: 1rem;
+			width: 3rem;
+			height: 3rem;
+			border-radius: 100%;
+		}
 	}
 
 	.timeline-container {
@@ -96,6 +169,7 @@
 	.reset {
 		position: absolute;
 		top: 3rem;
+		z-index: 101;
 	}
 
 	.timeline {
@@ -110,7 +184,7 @@
 		left: 0;
 		top: 0;
 		transform: translateY(-50%);
-		z-index: 100;
+		z-index: 101;
 		padding-left: 0.5rem;
 	}
 
